786994bc4ca83f59e33a67f47dafa8e13b6c46a8
Aidan Feuerherm <aidan.feuerherm@fullscript.com>
2023-12-09 10:13:13 -0500

    day_9

---
+++ day_9/part_1.rb
@@ -0,0 +1,32 @@
+require_relative "../utils/part"
+
+module Day9
+  class Part1 < Part
+    def call
+      puts @inputs.map(&method(:lagrange)).sum
+    end
+
+
+    def lagrange(points)
+      target_x = points.length
+      value = 0
+      points.each_with_index do |y, x|
+        numberator = 1
+        denominator = 1
+        for j in 0..target_x-1
+          next if j == x
+
+          numberator *= target_x - j
+          denominator *= x - j
+        end
+        value += y * numberator / denominator
+      end
+      value
+    end
+
+    def parse_input
+      @inputs = @file_lines.map{|line| line.split(" ").map(&:to_i)}
+    end
+
+  end
+end
